618b54c4fc61ca4613d0240fc4a6ccfeeddacbdf
slong <zoharslong@hotmail.com>
2020-01-15 11:22:42 +0800

    add init.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 22 16:30:38 2019
+@author: zoharslong
+"""
+if __name__ == '__main__':
+    print('running as a new program.')
+else:
+    print('pyzohar initiating succeeded.')
